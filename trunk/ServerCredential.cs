30947f2b52ad6f4e7bf35211c46d0fcf1f18dc9b
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-06-19 03:12:40 +0000

    Helper classes to provide client-aspect usage and server-aspect usage of the API.

---
+++ trunk/ServerCredential.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSspi
+{
+    public class ServerCredential : Credential
+    {
+        public ServerCredential( SecurityPackage package ) : base( package, CredentialType.Server ) { }
+    }
+}
