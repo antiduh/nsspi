641d4a2f868122f1816873b80a2da54e859bbf23
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-06-19 21:53:34 +0000

    Working on some of the fundamentals of the Context side of things. The Win 32 API is very wonky here though. Going to take some time.

---
+++ trunk/Contexts/Context.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSspi
+{
+    public class Context : IDisposable
+    {
+        private bool disposed;
+
+        public Context( Credential cred )
+        {
+            this.Credential = cred;
+
+            this.disposed = false;
+        }
+
+        ~Context()
+        {
+            Dispose( false );
+        }
+
+        protected Credential Credential { get; private set; }
+
+        public long ContextHandle { get; protected set; }
+
+        public void Dispose()
+        {
+            Dispose( true );
+            GC.SuppressFinalize( this );
+        }
+
+        protected virtual void Dispose( bool disposing )
+        {
+            if( this.disposed ) { return; }
+
+            if( disposing )
+            {
+                this.Credential.Dispose();
+            }
+
+            long contextHandleCopy = this.ContextHandle;
+            NativeMethods.DeleteSecurityContext( ref contextHandleCopy );
+
+            this.ContextHandle = 0;
+
+            this.disposed = true;
+        }
+    }
+}
---
+++ trunk/Contexts/ContextAttrib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSspi.Contexts
+{
+    [Flags]
+    public enum ContextReqAttrib : int 
+    {
+        None = 0,
+        Delegate = 1,
+        Identify = 2,
+        MutualAuth = 4,
+    }
+
+    public enum ContextResultAttrib : int
+    {
+    }
+}
