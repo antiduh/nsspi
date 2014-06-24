970c0f2bfaa2c5088506f6f2f6b07b7e2b4eb481
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-06-24 20:21:09 +0000

    Moved the safe handles for credentials and contexts into their own file.

---
+++ Contexts/SafeContextHandle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSspi.Contexts
+{
+
+    public class SafeContextHandle : SafeSspiHandle
+    {
+        public SafeContextHandle()
+            : base()
+        { }
+
+        protected override bool ReleaseHandle()
+        {
+            SecurityStatus status = ContextNativeMethods.DeleteSecurityContext(
+                ref base.rawHandle
+            );
+
+            base.ReleaseHandle();
+
+            return status == SecurityStatus.OK;
+        }
+    }
+}
