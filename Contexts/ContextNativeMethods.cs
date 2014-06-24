56e48dc65d499c29a48e1b90193ef75168d7b100
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-06-24 21:35:32 +0000

    Cleaned up code style and comments

--- Contexts/ContextNativeMethods.cs
+++ Contexts/ContextNativeMethods.cs
@@ -22,15 +22,24 @@ namespace NSspi
           _Out_        PULONG pfContextAttr,
           _Out_opt_    PTimeStamp ptsTimeStamp
         );
+         
+        SECURITY_STATUS SEC_Entry InitializeSecurityContext(
+          _In_opt_     PCredHandle phCredential,                // [in] handle to the credentials
+          _In_opt_     PCtxtHandle phContext,                   // [in/out] handle of partially formed context. Always NULL the first time through
+          _In_opt_     SEC_CHAR *pszTargetName,                 // [in] name of the target of the context. Not needed by NTLM
+          _In_         ULONG fContextReq,                       // [in] required context attributes
+          _In_         ULONG Reserved1,                         // [reserved] reserved; must be zero
+          _In_         ULONG TargetDataRep,                     // [in] data representation on the target
+          _In_opt_     PSecBufferDesc pInput,                   // [in/out] pointer to the input buffers.  Always NULL the first time through
+          _In_         ULONG Reserved2,                         // [reserved] reserved; must be zero
+          _Inout_opt_  PCtxtHandle phNewContext,                // [in/out] receives the new context handle (must be pre-allocated)
+          _Inout_opt_  PSecBufferDesc pOutput,                  // [out] pointer to the output buffers
+          _Out_        PULONG pfContextAttr,                    // [out] receives the context attributes
+          _Out_opt_    PTimeStamp ptsExpiry                     // [out] receives the life span of the security context
+        );
         */
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "AcceptSecurityContext",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode,
-            SetLastError = true
-        )]
+        [DllImport( "Secur32.dll", EntryPoint = "AcceptSecurityContext",CharSet = CharSet.Unicode )]
         public static extern SecurityStatus AcceptSecurityContext_1(
             ref RawSspiHandle credHandle,
             IntPtr oldContextHandle,
@@ -44,13 +53,7 @@ namespace NSspi
         );
 
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "AcceptSecurityContext",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode,
-            SetLastError = true
-        )]
+        [DllImport( "Secur32.dll", EntryPoint = "AcceptSecurityContext", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus AcceptSecurityContext_2(
             ref RawSspiHandle credHandle,
             ref RawSspiHandle oldContextHandle,
@@ -63,47 +66,8 @@ namespace NSspi
             ref long expiry
         );
 
-        // When used in the ClientContext:
-        /*
-			SECURITY_STATUS sResult = InitializeSecurityContext(
-				phCredential,										// [in] handle to the credentials
-				NULL,												// [in/out] handle of partially formed context. Always NULL the first time through
-				pwszServerPrincipalName,							// [in] name of the target of the context. Not needed by NTLM
-				reqContextAttributes,								// [in] required context attributes
-				0,													// [reserved] reserved; must be zero
-				SECURITY_NATIVE_DREP,								// [in] data representation on the target
-				NULL,												// [in/out] pointer to the input buffers.  Always NULL the first time through
-				0,													// [reserved] reserved; must be zero
-				this->contextHandle,								// [in/out] receives the new context handle (must be pre-allocated)
-				&outBuffDesc,										// [out] pointer to the output buffers
-				pulContextAttributes,								// [out] receives the context attributes
-				&tsLifeSpan											// [out] receives the life span of the security context
-			);
-        */
-        /*
-        SECURITY_STATUS SEC_Entry InitializeSecurityContext(
-          _In_opt_     PCredHandle phCredential,
-          _In_opt_     PCtxtHandle phContext,
-          _In_opt_     SEC_CHAR *pszTargetName,
-          _In_         ULONG fContextReq,
-          _In_         ULONG Reserved1,
-          _In_         ULONG TargetDataRep,
-          _In_opt_     PSecBufferDesc pInput,
-          _In_         ULONG Reserved2,
-          _Inout_opt_  PCtxtHandle phNewContext,
-          _Inout_opt_  PSecBufferDesc pOutput,
-          _Out_        PULONG pfContextAttr,
-          _Out_opt_    PTimeStamp ptsExpiry
-        );
-        */
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "InitializeSecurityContext",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode,
-            SetLastError = true
-        )]
+        [DllImport( "Secur32.dll", EntryPoint = "InitializeSecurityContext", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus InitializeSecurityContext_1(
             ref RawSspiHandle credentialHandle,
             IntPtr zero,
@@ -119,13 +83,8 @@ namespace NSspi
             ref long expiry
         );
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "InitializeSecurityContext",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode,
-            SetLastError = true
-        )]
+
+        [DllImport( "Secur32.dll", EntryPoint = "InitializeSecurityContext", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus InitializeSecurityContext_2(
             ref RawSspiHandle credentialHandle,
             ref RawSspiHandle previousHandle,
@@ -141,22 +100,12 @@ namespace NSspi
             ref long expiry
         );
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "DeleteSecurityContext",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode,
-            SetLastError = true
-        )]
+
+        [DllImport( "Secur32.dll", EntryPoint = "DeleteSecurityContext", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus DeleteSecurityContext( ref RawSspiHandle contextHandle );
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "EncryptMessage",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode,
-            SetLastError = true
-        )]
+
+        [DllImport( "Secur32.dll", EntryPoint = "EncryptMessage", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus EncryptMessage(
             ref RawSspiHandle contextHandle,
             int qualityOfProtection,
@@ -165,13 +114,7 @@ namespace NSspi
         );
 
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "DecryptMessage",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode,
-            SetLastError = true
-        )]
+        [DllImport( "Secur32.dll", EntryPoint = "DecryptMessage", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus DecryptMessage(
             ref RawSspiHandle contextHandle,
             IntPtr bufferDescriptor,
@@ -179,22 +122,14 @@ namespace NSspi
             int qualityOfProtection
         );
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "QueryContextAttributes",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode )]
+        [DllImport( "Secur32.dll", EntryPoint = "QueryContextAttributes", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus QueryContextAttributes_Sizes(
             ref RawSspiHandle contextHandle,
             ContextQueryAttrib attrib,
             ref SecPkgContext_Sizes sizes
         );
 
-        [DllImport(
-            "Secur32.dll",
-            EntryPoint = "QueryContextAttributes",
-            CallingConvention = CallingConvention.Winapi,
-            CharSet = CharSet.Unicode )]
+        [DllImport( "Secur32.dll", EntryPoint = "QueryContextAttributes", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus QueryContextAttributes_String(
             ref RawSspiHandle contextHandle,
             ContextQueryAttrib attrib,
@@ -202,11 +137,7 @@ namespace NSspi
         );
 
 
-        [DllImport( 
-            "Secur32.dll",
-            EntryPoint = "FreeContextBuffer", 
-            CallingConvention = CallingConvention.Winapi, 
-            CharSet = CharSet.Unicode )]
+        [DllImport( "Secur32.dll", EntryPoint = "FreeContextBuffer", CharSet = CharSet.Unicode )]
         public static extern SecurityStatus FreeContextBuffer( IntPtr handle );
     }
 }
