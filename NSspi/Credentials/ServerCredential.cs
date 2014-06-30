5da12ad4c4194d48e2486e14446afcac699d4686
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-06-30 15:32:31 +0000

    Split the Credential init code off into each concrete credential.

--- NSspi/Credentials/ServerCredential.cs
+++ NSspi/Credentials/ServerCredential.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,73 @@ namespace NSspi.Credentials
 {
     public class ServerCredential : Credential
     {
-        public ServerCredential( SecurityPackage package ) : base( package, CredentialType.Server ) { }
+        public ServerCredential( SecurityPackage package )
+            : base( package )
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            string packageName;
+            CredentialUse use;
+            TimeStamp rawExpiry = new TimeStamp();
+
+            // -- Package --
+            if( this.SecurityPackage == SecurityPackage.Kerberos )
+            {
+                packageName = PackageNames.Kerberos;
+            }
+            else if( this.SecurityPackage == SecurityPackage.Negotiate )
+            {
+                packageName = PackageNames.Negotiate;
+            }
+            else if( this.SecurityPackage == SecurityPackage.NTLM )
+            {
+                packageName = PackageNames.Ntlm;
+            }
+            else
+            {
+                throw new ArgumentException( "Invalid value provided for the 'package' parameter." );
+            }
+
+            // -- Credential --
+            // Server uses Inbound credentials.
+            use = CredentialUse.Inbound;
+
+            // -- Invoke --
+
+            SecurityStatus status = SecurityStatus.InternalError;
+
+            this.Handle = new SafeCredentialHandle();
+
+            // The finally clause is the actual constrained region. The VM pre-allocates any stack space,
+            // performs any allocations it needs to prepare methods for execution, and postpones any 
+            // instances of the 'uncatchable' exceptions (ThreadAbort, StackOverflow, OutOfMemory).
+            RuntimeHelpers.PrepareConstrainedRegions();
+            try { }
+            finally
+            {
+                status = CredentialNativeMethods.AcquireCredentialsHandle(
+                   null,
+                   packageName,
+                   use,
+                   IntPtr.Zero,
+                   IntPtr.Zero,
+                   IntPtr.Zero,
+                   IntPtr.Zero,
+                   ref this.Handle.rawHandle,
+                   ref rawExpiry
+               );
+            }
+
+            if( status != SecurityStatus.OK )
+            {
+                throw new SSPIException( "Failed to call AcquireCredentialHandle", status );
+            }
+
+            this.Expiry = rawExpiry.ToDateTime();
+        }
+
     }
 }
