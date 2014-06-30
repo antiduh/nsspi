9d0403ead60f1be8b54f164291ca4f17d9267b24
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-06-30 15:17:51 +0000

    More ReliabilityContracts for finalizer code.

--- NSspi/Contexts/SafeContextHandle.cs
+++ NSspi/Contexts/SafeContextHandle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace NSspi.Contexts
             : base()
         { }
 
+        [ReliabilityContract( Consistency.WillNotCorruptState, Cer.Success )]
         protected override bool ReleaseHandle()
         {
             SecurityStatus status = ContextNativeMethods.DeleteSecurityContext(
