894eb6016150c2f5392c967d1256925413b86af7
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-07-01 15:57:58 +0000

    Moved the simple demo code to its own project so that the NSspi.csproj project could be a standard dll, and not a referenced exe assembly.

--- NsspiDemo/Program.cs
+++ NsspiDemo/Program.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Security.Principal;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 using NSspi.Contexts;
 using NSspi.Credentials;
 
