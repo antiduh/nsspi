1158a559c642cdafd7cec3ad90420dd3ab828b3a
antiduh <antiduh@e114e3f1-24f7-e311-a78f-001bfcf5e160>
2014-07-05 14:32:04 +0000

    Fixed busted namespaces.

--- trunk/NSspi/Credentials/CredentialNativeMethods.cs
+++ trunk/NSspi/Credentials/CredentialNativeMethods.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using NSspi.Credentials;
-using NSspi.Credentials.Credentials;
 
 namespace NSspi.Credentials
 {
