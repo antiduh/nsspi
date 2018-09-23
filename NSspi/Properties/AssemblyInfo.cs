6bf95f46eee442666dfdcbde81aa675021a9c39c
Kevin Thompson <antiduh@gmail.com>
2018-09-23 10:44:43 -0400

    Updated version information to v0.2.1

--- NSspi/Properties/AssemblyInfo.cs
+++ NSspi/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Kevin Thompson" )]
 [assembly: AssemblyProduct( "NSspi" )]
-[assembly: AssemblyCopyright( "Copyright © Kevin Thompson 2017" )]
+[assembly: AssemblyCopyright( "Copyright © Kevin Thompson 2018" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.2.0.0" )]
-[assembly: AssemblyFileVersion( "0.2.0.0" )]
+[assembly: AssemblyVersion( "0.2.1.0" )]
+[assembly: AssemblyFileVersion( "0.2.1.0" )]
